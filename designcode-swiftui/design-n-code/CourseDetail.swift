514d7431ed49008142e22fbda55a1282531f92aa
Igor Kim <igor.skh@gmail.com>
2020-12-21 12:24:38 +0100

    check overscroll, adapt for dark mode

--- designcode-swiftui/design-n-code/CourseDetail.swift
+++ designcode-swiftui/design-n-code/CourseDetail.swift
@@ -49,6 +49,7 @@ struct CourseDetail_Previews: PreviewProvider {
             activeView: .constant(CGSize.zero),
             course: courseData[0]
         )
+        .environment(\.colorScheme, .dark)
     }
 }
 
@@ -70,13 +71,16 @@ struct CourseTextView: View {
                 Text("This course is unlike any other. We care about design and want to make sure that you get better at it in the process. It was written for designers and developers who are passionate about collaborating and building real apps for iOS and macOS.")
                 
                 Text("While it's not one codebase for all apps, you learn once and can apply the techniques and controls to all platforms with incredible quality, consistency and performance. It's beginner-friendly, but it's also packed with design tricks and efficient workflows for building great user interfaces and interactions.")
+                
+                Text("While it's not one codebase for all apps, you learn once and can apply the techniques and controls to all platforms with incredible quality, consistency and performance. It's beginner-friendly, but it's also packed with design tricks and efficient workflows for building great user interfaces and interactions.")
             }
+            .padding(.bottom, 100)
             .padding(30)
         }
         .frame(maxWidth: show ? .infinity : screen.width - 60, maxHeight: show ? .infinity : 280, alignment: .top)
         .offset(y: show ? 460 + textScrollOffset.y*1.5 : 0)
         .padding(.bottom, show ? 460 + textScrollOffset.y*1.5 : 0)
-        .background(Color.white)
+        .background(Color("background1"))
         .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
         .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
         .opacity(show ? 1 : 0)
