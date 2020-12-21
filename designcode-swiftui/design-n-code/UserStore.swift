9a2d6ef45c7de697290981458de2508663d6bbc8
Igor Kim <igor.skh@gmail.com>
2020-12-21 12:24:20 +0100

    Create UserStore.swift

---
+++ designcode-swiftui/design-n-code/UserStore.swift
@@ -0,0 +1,18 @@
+//
+//  UserStore.swift
+//  design-n-code
+//
+//  Created by Igor Kim on 20.12.20.
+//
+
+import SwiftUI
+import Combine
+
+class UserStore: ObservableObject {
+    @Published var isLogged: Bool = UserDefaults.standard.bool(forKey: "isLogged") {
+        didSet {
+            UserDefaults.standard.set(self.isLogged, forKey: "isLogged")
+        }
+    }
+    @Published var showLogin = false
+}
