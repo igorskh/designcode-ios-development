65c4f57abaf7970fdbab20633f2afb1f3f2899b7
Igor Kim <igor.skh@gmail.com>
2020-12-23 20:01:19 +0100

    implement notification service

---
+++ designcode-swiftui/notification-service/NotificationService.swift
@@ -0,0 +1,37 @@
+//
+//  NotificationService.swift
+//  notification-service
+//
+//  Created by Igor Kim on 23.12.20.
+//
+
+import UserNotifications
+
+class NotificationService: UNNotificationServiceExtension {
+
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
+
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.contentHandler = contentHandler
+        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+        
+        if let bestAttemptContent = bestAttemptContent {
+            // Modify the notification content here...
+            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
+            bestAttemptContent.subtitle = "\(bestAttemptContent.subtitle) [modified]"
+            bestAttemptContent.body = "\(bestAttemptContent.body) [modified]"
+            
+            contentHandler(bestAttemptContent)
+        }
+    }
+    
+    override func serviceExtensionTimeWillExpire() {
+        // Called just before the extension will be terminated by the system.
+        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+            contentHandler(bestAttemptContent)
+        }
+    }
+
+}
