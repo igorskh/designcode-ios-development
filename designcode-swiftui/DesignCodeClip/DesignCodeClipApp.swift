c7046b801f3f433d94b2f7af037fdbdc938c02c7
Igor Kim <igor.skh@gmail.com>
2020-12-26 19:50:26 +0100

    add an empty app clip

---
+++ designcode-swiftui/DesignCodeClip/DesignCodeClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeClipApp.swift
+//  DesignCodeClip
+//
+//  Created by Igor Kim on 25.12.20.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
